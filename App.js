664545fb68d481b641526db0af983ddae484d649
Abdullah Zayed <github/abdullahzayed812>
2022-11-15 22:27:01 +0200

    upload synced flat list react-native animation

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { API_KEY, API_URL } from "./config/config";
+
+const SPACEING = 10;
+const IMAGE_SIZE = 80;
+
+const fetchImagesFromPexels = async () => {
+  const data = await fetch(API_URL, {
+    headers: {
+      Authorization: API_KEY,
+    },
+  });
+  const result = await data.json();
+  return result;
+};
+
+const { width, height } = Dimensions.get("screen");
 
 export default function App() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+  const [images, setImages] = React.useState([]);
+  const absoluteListRef = React.useRef();
+  const thumbListRef = React.useRef();
+
+  React.useEffect(() => {
+    const fetchImages = async () => {
+      const { photos } = await fetchImagesFromPexels();
+      setImages(photos);
+    };
+    fetchImages();
+  }, []);
+
+  if (!images) {
+    return <Text>Loading...!</Text>;
+  }
+
+  const scrollToActiveIndex = (index) => {
+    setActiveIndex(index);
+    absoluteListRef?.current?.scrollToOffset({
+      offset: index * width,
+      animated: true,
+    });
+    if (index * (IMAGE_SIZE + SPACEING) - IMAGE_SIZE / 2 > width / 2) {
+      thumbListRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + SPACEING) - width / 2 + IMAGE_SIZE / 2,
+        animated: true,
+      });
+    } else {
+      thumbListRef?.current?.scrollToOffset({
+        offset: 0,
+        animated: true,
+      });
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        ref={absoluteListRef}
+        data={images}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled={true}
+        bounces={false}
+        onMomentumScrollEnd={(event) =>
+          scrollToActiveIndex(
+            Math.floor(event.nativeEvent.contentOffset.x / width)
+          )
+        }
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width, height }}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={StyleSheet.absoluteFill}
+              />
+            </View>
+          );
+        }}
+      />
+      <FlatList
+        ref={thumbListRef}
+        data={images}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{ position: "absolute", bottom: IMAGE_SIZE }}
+        contentContainerStyle={{ paddingHorizontal: SPACEING }}
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableOpacity onPress={() => scrollToActiveIndex(index)}>
+              <Image
+                source={{ uri: item.src.portrait }}
+                style={{
+                  width: IMAGE_SIZE,
+                  height: IMAGE_SIZE,
+                  borderRadius: SPACEING,
+                  marginRight: SPACEING,
+                  borderWidth: 2,
+                  borderColor: index === activeIndex ? "#fff" : "transparent",
+                }}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const API_KEY =
+  "563492ad6f91700001000001d20a3a3408124a4ca696657cec633cb5";
+export const API_URL =
+  "https://api.pexels.com/v1/search?query=nature&orientation=portrait&size=small&per_page=20";
